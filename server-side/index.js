f5f866e362eb0740cd678a2060c34143a0d0dfe2
Ravi Kishan <kishanravi887321@gmail.com>
2025-11-19 01:17:50 +0530

    feat: refactor server setup and add dotenv for environment variable management

--- server-side/index.js
+++ server-side/index.js
@@ -1,21 +1,14 @@
-const express = require('express');
-const app = express();
+import  app from './src/app.js';
+import dotenv from 'dotenv';
+
+dotenv.config({path:'../.env'});
 
 const PORT = process.env.PORT || 3000;
 
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Routes
-app.get('/api/hello', (req, res) => {
-  res.json({
-    message: 'Hello from the API!',
-    timestamp: new Date().toISOString()
+const startServer = () => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
   });
-});
+};
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+startServer();
---
+++ server-side/src/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/api/hello', (req, res) => {
+  res.json({
+    message: 'Hello from the API!',
+    timestamp: new Date().toISOString()
+  });
+});
+
+
+
+export default app;
+
