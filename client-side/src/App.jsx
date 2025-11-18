4840d03ea4d889cbb98ca184af961c57a2459b4d
Ravi Kishan <kishanravi887321@gmail.com>
2025-11-19 02:35:20 +0530

    feat: add whitespace for improved code readability in App component

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -6,6 +6,7 @@ import './App.css';
 function App() {
   return (
     <Router>
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/room/:roomId" element={<Room />} />
